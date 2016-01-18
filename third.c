2f87cb39612e813af8944cad1abfb8d8cb789c99
saitej3 <saitej3@gmail.com>
2016-01-19 03:56:12 +0530

    pipes done

---
+++ third.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main()
+{
+	FILE * f;
+	f=popen("third",);
+	char buf[128s];
+	cin>>buf;
+	write(fd,"hello",128);
+}
---
+++ third.c~
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main()
+{
+	FILE * f;
+	f=popen("third",0);
+	char buf[128];
+	cin>>buf;
+	write(fd,"hello",128);
+}
